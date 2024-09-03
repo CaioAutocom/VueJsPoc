f6358946465248d93001ddb215378b5f40a36035
Caio Mendes <caio.mendes@autocom-mg.com.br>
2024-09-03 16:27:46 -0300

    added pinia instance

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 
+const pinia = createPinia();
 const app = createApp(App);
-app.use(PrimeVue, {
+app.use(
+    PrimeVue, 
+    pinia, {
     theme: {
         preset: Aura
     }
